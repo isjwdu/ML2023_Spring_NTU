84d01a5e5c06c36c569fbef6e1434b2c8783bc54
isjwdu <112316890+isjwdu@users.noreply.github.com>
2023-04-04 22:39:10 +0800

    Update hw4_with_SAPooling.py

--- HW4/hw4_with_SAPooling.py
+++ HW4/hw4_with_SAPooling.py
@@ -579,7 +579,7 @@ def parse_args():
     config = {
         "data_dir": "./Dataset",
         "model_path": "./model.ckpt",
-        "output_path": "./output.csv",
+        "output_path": "./output_with_SAP.csv",
     }
 
     return config
